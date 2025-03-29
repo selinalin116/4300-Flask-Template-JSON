61a9fe6fb385159c98f4b115d9a3301836ae8f74
vivianzhou1288 <vivianzhou1288@gmail.com>
2025-03-29 14:38:33 -0400

    Changed cocktails into individual cards

--- backend/app.py
+++ backend/app.py
@@ -113,8 +113,8 @@ def find_foods():
         "recipes": top_recipes
     })
 
-# if __name__ == '__main__':
-#     app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
-if 'DB_NAME' not in os.environ:
-    app.run(debug=True,host="0.0.0.0",port=5000)
+# if 'DB_NAME' not in os.environ:
+#     app.run(debug=True,host="0.0.0.0",port=5000)
