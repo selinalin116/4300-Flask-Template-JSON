dd1548190f07914aa67492b126395f9c1e9621d9
vivianzhou1288 <vz48@cornell.edu>
2025-04-13 23:56:30 -0400

    Modified dataset

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import helper_functions
+from collections import defaultdict
 
 os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 
